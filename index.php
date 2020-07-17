9007068a441395226ffd4acd49de20f6365f4505
rahmatulloheko141414 <68285711+rahmatulloheko141414@users.noreply.github.com>
2020-07-17 14:25:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RJP JOGJA</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container">
+		<button type="button" class="btn btn-success">wHATSAPP</button>
+		<hr>
+		<button type="button" class="btn btn-warning">sHOPEE</button>
+		<hr>
+		<button type="button" class="btn btn-danger">INSTAGRAM</button>
+	</div>
+</body>
+</html>
